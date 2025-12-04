9aa31e24ff9cd06a138c00c1d9855201ae82b275
mszahan <mszahan1@gmail.com>
2025-12-04 22:12:33 +0600

    activated product list page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,13 @@
+import { products } from "../product-data"
+import ProductList from "../ProductList"
+
 export default function ProductsPage() {
-    return <h1>Products</h1>
+    return (
+        <div>
+            <h1>Products</h1>
+            <ProductList products={products} />
+        </div>
+    )
+
+
 }
