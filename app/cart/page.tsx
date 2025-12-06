c32a147fa626fa767293bc725db01cd2c173c06d
mszahan <mszahan1@gmail.com>
2025-12-06 15:44:15 +0600

    activated cart and improved product detail page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,3 +1,23 @@
-export default function CartPage () {
-    return <h1>Cart</h1>
+'use client'
+
+import { useState } from "react"
+import Link from "next/link"
+import { products } from "../product-data"
+
+
+export default function CartPage() {
+
+    const [cartIds, setCartIds] = useState(['123', '345'])
+    const cartProducts = cartIds.map(id => products.find(p => p.id === id)!)
+    return (
+        <>
+            <h1>Shoping Cart</h1>
+            {cartProducts.map(product => (
+                <Link key={product.id} href={'products/' + product.id}>
+                    <h3> {product.name} </h3>
+                    <p>${product.price}</p>
+                </Link>
+            ))}
+        </>
+    )
 }
--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,5 +2,13 @@ import { products } from "@/app/product-data"
 export default async function ProductDetailPage({ params }: { params: Promise<{ id: string }> }) {
     const { id } = await params
     const product = products.find(p => p.id === id)
-    return <h1>{product?.name}</h1>
+    return (
+        <>
+            <h1>{product?.name}</h1>
+            <p>${product?.price}</p>
+            <p>Description: {product?.description}</p>
+        </>
+    )
+
+
 }
