f8b894232586e9cd589f17e6290f04c9ed2f4b45
mszahan <mszahan1@gmail.com>
2025-12-06 16:06:46 +0600

    customized not found page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,12 +1,16 @@
+import NotFoundPage from "@/app/not-found"
 import { products } from "@/app/product-data"
 export default async function ProductDetailPage({ params }: { params: Promise<{ id: string }> }) {
     const { id } = await params
     const product = products.find(p => p.id === id)
+    if (!product) {
+        return <NotFoundPage />
+    }
     return (
         <>
-            <h1>{product?.name}</h1>
-            <p>${product?.price}</p>
-            <p>Description: {product?.description}</p>
+            <h1>{product.name}</h1>
+            <p>${product.price}</p>
+            <p>Description: {product.description}</p>
         </>
     )
 
